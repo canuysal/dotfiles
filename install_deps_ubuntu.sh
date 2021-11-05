f127017f4cb0de1038ef5164721e367baaa19011
Can Uysal <can.uysal@sparsetechnology.com>
2021-11-05 11:41:19 +0300

    add polybar support.

--- install_deps_ubuntu.sh
+++ install_deps_ubuntu.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+sudo apt update
+
 sudo add-apt-repository -y ppa:regolith-linux/stable
 sudo apt install -y i3 i3-gaps feh rofi arandr
+
+# polybar
+sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
+sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
+sudo apt install libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
+
+git clone https://github.com/jaagr/polybar.git
+cd polybar && ./build.sh
